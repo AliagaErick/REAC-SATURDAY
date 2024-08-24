2b53b89271f80ce2a0ff8e4baee6d5fe48fd9275
AliagaErick <167391296+AliagaErick@users.noreply.github.com>
2024-08-24 23:38:37 +0000

    3er prueba

--- AppDemo/src/App.jsx
+++ AppDemo/src/App.jsx
@@ -1,13 +1,17 @@
 import { Fragment } from "react";
-
 import Mensaje from "./Componente/Mensaje";
 import Contador from "./Componente/Contador";
+import MiCard from "./Componente/Card";
+import Lista from "./Componente/Lista";
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 function App() {
   return (
     <>
       <Mensaje />
       <Contador />
+      <MiCard />
+      <Lista />
     </>
   );
 }
