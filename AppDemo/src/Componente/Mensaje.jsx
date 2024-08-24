568e2acf773521c8b00125dfd16239142a0bd254
AliagaErick <167391296+AliagaErick@users.noreply.github.com>
2024-08-24 21:46:51 +0000

    Mi primer react

---
+++ AppDemo/src/Componente/Mensaje.jsx
@@ -0,0 +1,13 @@
+import { Fragment } from "react"
+
+
+function Mensaje(){
+    return (
+        <>
+        <h1>Laboratorio de Ejercicios Práctico</h1>
+        <h4>Estudiante: Erick Aliaga</h4>
+        </>
+    )
+}
+
+export default Mensaje;
